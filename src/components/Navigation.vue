5eab0d0b433d5043635438674c2b7d6ef201778b
Phuoc Nguyen <phuoc94@hotmail.fi>
2023-07-07 13:59:32 +0300

    update nav

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,90 +1,101 @@
 <template>
-  <nav class="container mx-auto">
-    <div class="flex flex-row justify-end h-[76px] md:h-[122px]">
-      <div class="basis-1/3 self-center hidden md:block">
-        <ul class="flex">
-          <li
-            v-for="item in menuItems"
-            :key="item.id"
-            :class="{ 'active-menu': isActive(item) }"
-            class="mr-2"
-          >
-            <router-link
-              :to="item.route"
-              @click="activateItem(item)"
-              class="p-2"
-              >{{ item.label }}</router-link
-            >
-          </li>
-        </ul>
-      </div>
-      <div class="basis-1/3 flex justify-center">
-        <router-link to="/">
-          <img class="h-[57px] md:h-[85px]" src="@/assets/logo.png" />
-        </router-link>
-      </div>
-      <div
-        class="basis-1/3 self-center justify-end items-center gap-[39px] hidden md:flex"
+  <nav class="flex container mx-auto items-center flex-wrap px-6 justify-end">
+    <div
+      class="basis-5/12 gap-[39px] hidden justify-end items-center lg:order-last lg:flex"
+    >
+      <a href="https://www.facebook.com/VietnamilainenRavintolaKim">
+        <i class="icon pi pi-facebook"></i>
+      </a>
+      <router-link to="/contact#reservation">
+        <button class="book-table">Book a table</button>
+      </router-link>
+    </div>
+    <div
+      class="flex basis-2/12 items-center justify-center flex-shrink-0 pb-6 pt-1"
+    >
+      <router-link to="/">
+        <img class="h-[57px] lg:h-[85px]" src="@/assets/logo.png" />
+      </router-link>
+    </div>
+    <div class="flex basis-5/12 justify-end lg:hidden">
+      <button
+        @click="open = !open"
+        class="flex items-center px-3 py-2 border rounded"
       >
-        <a href="https://www.facebook.com/VietnamilainenRavintolaKim">
-          <i class="icon pi pi-facebook"></i>
-        </a>
-        <router-link to="/contact#reservation">
-          <button>Book a table</button>
+        <i v-if="!open" class="icon pi pi-bars"></i>
+        <i v-else class="icon pi pi-times"></i>
+      </button>
+    </div>
+    <div
+      :class="{ block: open, hidden: !open }"
+      class="w-full block flex-grow text-right lg:text-left lg:flex lg:items-center lg:w-auto lg:order-first"
+    >
+      <div class="text-sm lg:flex-grow">
+        <router-link
+          v-for="link in menuItems"
+          :key="link.id"
+          :to="link.route"
+          class="block mt-4 lg:inline-block lg:mt-0 mr-4"
+          active-class="router-link-active"
+        >
+          <span class="link">{{ link.label }}</span>
         </router-link>
       </div>
-
-      <div class="basis-1/3 self-center justify-end flex md:hidden pr-[17px]">
-        <i class="icon pi pi-bars"></i>
+      <div class="lg:hidden my-4">
+        <router-link to="/contact#reservation">
+          <button class="book-table">Book a table</button>
+        </router-link>
       </div>
     </div>
   </nav>
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref, watch } from "vue";
 import { useRoute } from "vue-router";
 import "primeicons/primeicons.css";
 
 export default {
   setup() {
-    const menuItems = ref([
-      { id: 1, label: "Menu", route: "/menu" },
-      { id: 2, label: "About", route: "/about" },
-      { id: 3, label: "Contact", route: "/contact" },
-      { id: 4, label: "Gallery", route: "/gallery" },
-    ]);
-
+    const open = ref(false);
     const route = useRoute();
 
-    const isActive = (item) => {
-      return item.route === route.path;
-    };
-
-    const activateItem = (item) => {
-      menuItems.value.forEach((menuItem) => {
-        menuItem.isActive = menuItem.id === item.id;
-      });
-    };
+    const menuItems = ref([
+      { id: 1, label: "Home", route: "/" },
+      { id: 2, label: "Menu", route: "/menu" },
+      { id: 3, label: "About", route: "/about" },
+      { id: 4, label: "Contact", route: "/contact" },
+      { id: 5, label: "Gallery", route: "/gallery" },
+    ]);
+    watch(route, () => {
+      open.value = false;
+    });
 
     return {
+      open,
       menuItems,
-      isActive,
-      activateItem,
     };
   },
 };
 </script>
 
 <style scoped>
-.active-menu {
-  color: #171717;
+.book-table {
+  background: var(--ffbb-00, #ffc933);
+  border-radius: 0.3125rem;
+  color: var(--171717, #171717);
+  font-family: David Libre;
+  font-size: 1rem;
+  font-style: normal;
   font-weight: 700;
+  line-height: 1.8125rem;
+  padding: 0.75rem 2rem;
 }
-li {
+
+.link {
   color: #101a24;
-  font-size: 24px;
   font-family: Times New Roman;
+  font-size: 24px;
   font-style: normal;
   font-weight: 400;
   line-height: 29px;
@@ -94,19 +105,9 @@ li {
   color: #000000;
   font-size: 36px;
 }
-.pi-bars {
-  color: #000000;
-  font-size: 24px;
-}
-button {
-  padding: 0.75rem 2rem;
-  border-radius: 0.3125rem;
-  background: var(--ffbb-00, #ffc933);
-  color: var(--171717, #171717);
-  font-size: 1rem;
-  font-family: David Libre;
-  font-style: normal;
-  font-weight: 700;
-  line-height: 1.8125rem;
+
+.router-link-active > .link {
+  color: #171717 !important;
+  font-weight: 700 !important;
 }
 </style>
