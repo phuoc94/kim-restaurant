db944af0a7de2d0bdb848f0f03379c7b6c33064e
Phuoc Nguyen <phuoc94@hotmail.fi>
2023-07-21 10:43:31 +0300

    split component

---
+++ src/components/BestSeller.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="flex flex-wrap justify-center gap-y-12 py-8 sm:px-12">
+    <div
+      v-for="(card, index) in cards"
+      :key="index"
+      class="basis-full px-4 sm:basis-1/2 md:basis-1/3 lg:basis-1/4"
+    >
+      <Card
+        :title="card.title"
+        :description="card.description"
+        :price="card.price"
+        :image="card.image"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { reactive } from "vue";
+import Card from "@/components/BestSellerCard.vue";
+
+export default {
+  components: { Card },
+  setup() {
+    const cards = reactive([
+      {
+        title: "Gỏi cuốn",
+        description:
+          "Sekoitus salaattia, paprikaa, kurkkua, riisinuudeleita, hoisin-kastiketta ja maapähkinöitä",
+        price: "7,50",
+        image: require("@/assets/mihoanhthanh.png"),
+      },
+      {
+        title: "Rapeat katkaravut",
+        description:
+          "Friteerattu katkarapu salaatin, tomaatin ja hapanimeläkastikkeen kera",
+        price: "13,50",
+        image: require("@/assets/mihoanhthanh.png"),
+      },
+      {
+        title: "Phở",
+        description:
+          "Perinteisiä vietnamilaisia nuudeleita kanan ja naudanlihan kera",
+        price: "13,50",
+        image: require("@/assets/mihoanhthanh.png"),
+      },
+      {
+        title: "Hủ tiếu xào",
+        description: "Riisinuudeliwokki vihannesten kera",
+        price: "7,50",
+        image: require("@/assets/mihoanhthanh.png"),
+      },
+      {
+        title: "Curry",
+        description:
+          "Keltainen curry/ Punainen curry/ Vihreä curry/ Massaman-curry/ Tom kha",
+        price: "13,50",
+        image: require("@/assets/mihoanhthanh.png"),
+      },
+      {
+        title: "Gỏi cuốn",
+        description:
+          "Sekoitus salaattia, paprikaa, kurkkua, riisinuudeleita, hoisin-kastiketta ja maapähkinöitä",
+        price: "7,50",
+        image: require("@/assets/mihoanhthanh.png"),
+      },
+      {
+        title: "Gỏi cuốn",
+        description:
+          "Sekoitus salaattia, paprikaa, kurkkua, riisinuudeleita, hoisin-kastiketta ja maapähkinöitä",
+        price: "7,50",
+        image: require("@/assets/mihoanhthanh.png"),
+      },
+    ]);
+
+    return { cards };
+  },
+};
+</script>
+
+<style scoped></style>
--- src/views/MenuView.vue
+++ src/views/MenuView.vue
@@ -21,23 +21,7 @@
           {{ tab.label }}
         </div>
       </div>
-      <div
-        v-if="currentTab === 'tab1'"
-        class="flex flex-wrap justify-center gap-y-12 py-8 sm:px-12"
-      >
-        <div
-          v-for="(card, index) in cards"
-          :key="index"
-          class="basis-full px-4 sm:basis-1/2 md:basis-1/3 lg:basis-1/4"
-        >
-          <Card
-            :title="card.title"
-            :description="card.description"
-            :price="card.price"
-            :image="card.image"
-          />
-        </div>
-      </div>
+      <div v-if="currentTab === 'tab1'"><BestSeller /></div>
       <div v-else-if="currentTab === 'tab2'">Content for Tab 2</div>
       <div v-else-if="currentTab === 'tab3'">Content for Tab 3</div>
       <div v-else><FrameFour /></div>
@@ -46,12 +30,12 @@
 </template>
 
 <script>
-import { ref, reactive } from "vue";
+import { ref } from "vue";
 import FrameFour from "@/components/FrameFour.vue";
-import Card from "@/components/BestSellerCard.vue";
+import BestSeller from "@/components/BestSeller.vue";
 
 export default {
-  components: { FrameFour, Card },
+  components: { FrameFour, BestSeller },
   setup() {
     const tabs = ref([
       { id: "tab1", label: "Bestseller" },
@@ -65,59 +49,7 @@ export default {
       currentTab.value = tab;
     };
 
-    const cards = reactive([
-      {
-        title: "Gỏi cuốn",
-        description:
-          "Sekoitus salaattia, paprikaa, kurkkua, riisinuudeleita, hoisin-kastiketta ja maapähkinöitä",
-        price: "7,50",
-        image: require("@/assets/mihoanhthanh.png"),
-      },
-      {
-        title: "Rapeat katkaravut",
-        description:
-          "Friteerattu katkarapu salaatin, tomaatin ja hapanimeläkastikkeen kera",
-        price: "13,50",
-        image: require("@/assets/mihoanhthanh.png"),
-      },
-      {
-        title: "Phở",
-        description:
-          "Perinteisiä vietnamilaisia nuudeleita kanan ja naudanlihan kera",
-        price: "13,50",
-        image: require("@/assets/mihoanhthanh.png"),
-      },
-      {
-        title: "Hủ tiếu xào",
-        description: "Riisinuudeliwokki vihannesten kera",
-        price: "7,50",
-        image: require("@/assets/mihoanhthanh.png"),
-      },
-      {
-        title: "Curry",
-        description:
-          "Keltainen curry/ Punainen curry/ Vihreä curry/ Massaman-curry/ Tom kha",
-        price: "13,50",
-        image: require("@/assets/mihoanhthanh.png"),
-      },
-      {
-        title: "Gỏi cuốn",
-        description:
-          "Sekoitus salaattia, paprikaa, kurkkua, riisinuudeleita, hoisin-kastiketta ja maapähkinöitä",
-        price: "7,50",
-        image: require("@/assets/mihoanhthanh.png"),
-      },
-      {
-        title: "Gỏi cuốn",
-        description:
-          "Sekoitus salaattia, paprikaa, kurkkua, riisinuudeleita, hoisin-kastiketta ja maapähkinöitä",
-        price: "7,50",
-        image: require("@/assets/mihoanhthanh.png"),
-      },
-    ]);
-
     return {
-      cards,
       tabs,
       currentTab,
       changeTab,
