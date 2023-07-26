860ea08175852a9f046240decb9daacd1e029194
Phuoc Nguyen <phuoc94@hotmail.fi>
2023-07-26 17:09:50 +0300

    added lightbox to gallery

---
+++ src/components/Lightbox.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+    class="fixed top-0 flex h-screen w-full items-center justify-center bg-black/80"
+    @click.self="$emit('close')"
+  >
+    <div class="relative z-0 m-4 max-h-[90vh] bg-white md:max-h-[80vh]">
+      <img
+        :src="selectedImage"
+        class="h-full max-h-[90vh] w-full object-cover md:max-h-[80vh]"
+      />
+      <button
+        class="absolute right-4 top-4 z-10 p-2"
+        @click.stop="$emit('close')"
+      >
+        <i class="icon pi pi-times text-xl"></i>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["selectedImage"],
+};
+</script>
+
+<style></style>
--- src/views/GalleryView.vue
+++ src/views/GalleryView.vue
@@ -6,34 +6,85 @@
       </h1>
       <div class="flex justify-center px-4">
         <div class="grid grid-cols-[auto_auto_auto] gap-2">
-          <div class="col-span-2 h-full">
-            <img class="h-full w-full object-cover" src="@/assets/grid1.png" />
+          <div
+            class="col-span-2 h-full cursor-pointer"
+            @click="toggleLightbox(grid1)"
+          >
+            <img class="h-full w-full object-cover" :src="grid1" />
           </div>
-          <div class="row-span-2 h-full">
-            <img class="h-full w-full object-cover" src="@/assets/grid5.png" />
+          <div
+            class="row-span-2 h-full cursor-pointer"
+            @click="toggleLightbox(grid5)"
+          >
+            <img class="h-full w-full object-cover" :src="grid5" />
           </div>
-
-          <div class="col-span-2 h-full">
-            <img class="h-full w-full object-cover" src="@/assets/grid2.png" />
+          <div
+            class="col-span-2 h-full cursor-pointer"
+            @click="toggleLightbox(grid2)"
+          >
+            <img class="h-full w-full object-cover" :src="grid2" />
           </div>
-
-          <div class="h-full">
-            <img class="h-full w-full object-cover" src="@/assets/grid3.png" />
+          <div class="h-full" @click="toggleLightbox(grid3)">
+            <img
+              class="h-full w-full cursor-pointer object-cover"
+              :src="grid3"
+            />
           </div>
-          <div class="col-span-2 h-full">
-            <img class="h-full w-full object-cover" src="@/assets/grid4.png" />
+          <div class="col-span-2 h-full" @click="toggleLightbox(grid4)">
+            <img
+              class="h-full w-full cursor-pointer object-cover"
+              :src="grid4"
+            />
           </div>
         </div>
       </div>
     </div>
   </div>
   <FrameFour />
+  <Lightbox
+    v-if="showLightbox"
+    :selectedImage="selectedImage"
+    @close="toggleLightbox"
+  />
 </template>
 
-<script lang="ts">
+<script>
+import { reactive, toRefs, onUnmounted } from "vue";
 import FrameFour from "@/components/FrameFour.vue";
+import Lightbox from "@/components/Lightbox.vue";
+import grid1 from "@/assets/grid1.webp";
+import grid2 from "@/assets/grid2.png";
+import grid3 from "@/assets/grid3.png";
+import grid4 from "@/assets/grid4.png";
+import grid5 from "@/assets/grid5.png";
 
 export default {
-  components: { FrameFour },
+  components: { FrameFour, Lightbox },
+  setup() {
+    const state = reactive({
+      showLightbox: false,
+      selectedImage: null,
+    });
+
+    function toggleLightbox(imageSrc = null) {
+      state.selectedImage = imageSrc;
+      state.showLightbox = !!imageSrc;
+      document.body.style.overflow = state.showLightbox ? "hidden" : "";
+    }
+
+    onUnmounted(() => {
+      document.body.style.overflow = "";
+    });
+
+    return {
+      ...toRefs(state),
+      toggleLightbox,
+      grid1,
+      grid2,
+      grid3,
+      grid4,
+      grid5,
+    };
+  },
 };
 </script>
