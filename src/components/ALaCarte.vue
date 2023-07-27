0e3377daa91e2b8562feccca8bb18a9297407234
Phuoc Nguyen <phuoc94@hotmail.fi>
2023-07-28 00:32:26 +0300

    update alacarte

--- src/components/ALaCarte.vue
+++ src/components/ALaCarte.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
+    <div v-if="error" class="p-64 text-center">
+      An error occurred: {{ error }}
+    </div>
+
+    <div v-else-if="loading" class="p-64 text-center">Loading...</div>
+
     <div
+      v-else
       v-for="(dishes, category) in groupedDishes"
       :key="category"
       class="bg-gradient-to-r from-yellow-300/70 to-orange-400/70"
@@ -16,13 +23,14 @@
           :key="index"
         >
           <div class="grid-feature row-span-3 lg:row-span-2">
-            <span v-if="dish.featured">
+            <span v-if="dish.bestSellerIcon">
               <img src="@/assets/featured-icon.webp" />
             </span>
           </div>
           <div class="grid-title">
             <h3 class="text-xl md:text-2xl lg:font-bold">
-              {{ dish.dishNo }}. {{ dish.title }}
+              <span v-if="dish.dishNumber">{{ dish.dishNumber }}.</span>
+              {{ dish.title }}
             </h3>
           </div>
           <div
@@ -35,18 +43,58 @@
           <div
             class="grid-dietary col-start-3 row-start-1 lg:col-start-3 lg:row-start-1"
           >
-            <p class="text-xl md:text-2xl lg:font-bold">
-              {{ dish.dietary }}
-            </p>
+            <div class="absolute flex">
+              <div v-if="dish.vegan" class="relative mt-[1px] md:mt-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                >
+                  <path
+                    d="M15 11.063C12.53 13.65 10.059 20 10.059 20C10.059 20 6.529 11.062 3 9"
+                    stroke="#629614"
+                    stroke-width="1.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M20.4958 5.57701L20.9218 10.001C21.1978 12.871 19.0468 15.426 16.1768 15.703C13.3608 15.973 10.8098 13.915 10.5388 11.099C10.4742 10.4294 10.5422 9.75361 10.7389 9.11026C10.9355 8.46691 11.257 7.86861 11.6849 7.34951C12.1128 6.83041 12.6387 6.4007 13.2327 6.0849C13.8267 5.76911 14.4771 5.57342 15.1468 5.50901L19.8628 5.05501C19.9386 5.0477 20.0152 5.05541 20.088 5.07769C20.1609 5.09997 20.2286 5.13639 20.2874 5.18486C20.3462 5.23334 20.3949 5.29292 20.4306 5.36021C20.4664 5.4275 20.4885 5.50117 20.4958 5.57701Z"
+                    stroke="#629614"
+                    stroke-width="1.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </div>
+              <p class="text-xl md:text-2xl lg:font-bold">
+                {{ dish.dietary }}
+              </p>
+              <div v-if="dish.hot" class="relative mt-[1px] md:mt-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                >
+                  <path
+                    d="M16 10V22C16 22 8 20 8 11V10C8 9.27 8.4 8.63 9 8.28L10.25 9L12 8L13.75 9L15 8.28C15.6 8.63 16 9.27 16 10ZM12 6.5L13.75 7.5L15.27 6.63C14.72 5.66 13.91 4.94 12.97 4.65C12.8853 3.92126 12.5361 3.24894 11.9887 2.7605C11.4413 2.27206 10.7336 2.00146 10 2V4C10.44 4 10.8 4.29 10.94 4.69C10.03 5 9.26 5.7 8.73 6.63L10.25 7.5L12 6.5Z"
+                    fill="#D30000"
+                  />
+                </svg>
+              </div>
+            </div>
           </div>
           <div
             class="grid-price col-span-2 col-start-2 row-start-2 font-bold md:text-xl lg:col-span-1 lg:col-start-4 lg:row-span-2 lg:row-start-1"
           >
             <div v-for="(price, priceIndex) in dish.prices" :key="priceIndex">
               <p v-if="dish.prices.length > 1">
-                {{ price.title }}: {{ price.price }}
+                {{ price.type }}: {{ price.price.toFixed(2) }}
               </p>
-              <p v-else>{{ price.price }}</p>
+              <p v-else>{{ price.price.toFixed(2) }}</p>
             </div>
           </div>
         </div>
@@ -55,245 +103,80 @@
   </div>
 </template>
 
-<script>
-import { ref, computed } from "vue";
-export default {
-  setup() {
-    const Dishes = ref([
-      {
-        featured: true,
-        dishNo: 1,
-        title: "Goi cuôn",
-        description:
-          "Sekoitus salaattia, paprikaa, kurkkua, riisinuudeleita, hoisin- kastiketta ia maapähkinöitä",
-        prices: [
-          { title: "Tofu", price: "6,5" },
-          { title: "Katkarapu", price: "7,5" },
-        ],
-        dietary: "L, G",
-        image: null,
-        category: "Alkuruoat",
-      },
-      {
-        featured: true,
-        dishNo: 1,
-        title: "Rapeat katkaravut",
-        description:
-          "Friteerattu katkarapu salaatin, tomaatin ja hapanimeläkastikkeen kera",
-        prices: [
-          {
-            title: null,
-            price: "7,50",
-          },
-        ],
-        dietary: "L, G",
-        image: null,
-        category: "Alkuruoat",
-      },
+<script setup>
+import { ref, onMounted, computed } from "vue";
+import axios from "axios";
+const API_URL = process.env.VUE_APP_API_URL;
 
-      {
-        featured: true,
-        dishNo: 1,
-        title: "Pho",
-        description:
-          "Perinteisia vietnamilaisia nuudeleita kanan a naudanlihan kera",
-        prices: [
-          {
-            title: null,
-            price: "14,50",
-          },
-        ],
-        dietary: "L, G",
-        image: null,
-        category: "Vietnamilaiset Erikoisuudet",
-      },
-      {
-        featured: true,
-        dishNo: 1,
+const dishes = ref([]);
+const category = ref([]);
+const loading = ref(true);
+const error = ref(null);
 
-        title: "Pho",
-        description:
-          "Perinteisia vietnamilaisia nuudeleita kanan a naudanlihan kera",
-        prices: [
-          {
-            title: null,
-            price: "14,50",
-          },
-        ],
-        dietary: "L, G",
-        image: null,
-        category: "Vietnamilaiset Erikoisuudet",
-      },
-      {
-        featured: true,
-        dishNo: 1,
+const sortedCategory = computed(() => {
+  return [...category.value].sort((a, b) => a.sequence - b.sequence);
+});
 
-        title: "Pho",
-        description:
-          "Perinteisia vietnamilaisia nuudeleita kanan a naudanlihan kera",
-        prices: [
-          {
-            title: null,
-            price: "14,50",
-          },
-        ],
-        dietary: "L, G",
-        image: null,
-        category: "Vietnamilaiset Erikoisuudet",
-      },
-      {
-        featured: true,
-        dishNo: 1,
+const groupedDishes = computed(() => {
+  let grouped = {};
+  sortedCategory.value.forEach((cat) => {
+    grouped[cat.title] = dishes.value.filter(
+      (dish) => dish.category === cat.categoryKey
+    );
+  });
+  return grouped;
+});
 
-        title: "Pho",
-        description:
-          "Perinteisia vietnamilaisia nuudeleita kanan a naudanlihan kera",
-        prices: [
-          {
-            title: null,
-            price: "14,50",
-          },
-        ],
-        dietary: "L, G",
-        image: null,
-        category: "Vietnamilaiset Erikoisuudet",
-      },
-      {
-        featured: false,
-        dishNo: 1,
-
-        title: "Bò xão thom",
-        description: "Naudanliha ananaksen kera",
-        prices: [
-          {
-            title: null,
-            price: "15,90",
-          },
-        ],
-        dietary: "L, G",
-        image: null,
-        category: "Naudanliharuoat",
-      },
-      {
-        featured: false,
-        dishNo: 1,
-
-        title: "Bò xão thom",
-        description: "Naudanliha ananaksen kera",
-        prices: [
-          {
-            title: null,
-            price: "15,90",
-          },
-        ],
-        dietary: "L, G",
-        image: null,
-        category: "Naudanliharuoat",
-      },
-      {
-        featured: false,
-        dishNo: 1,
-
-        title: "Bò xão thom",
-        description: "Naudanliha ananaksen kera",
-        prices: [
-          {
-            title: null,
-            price: "15,90",
-          },
-        ],
-        dietary: "L, G",
-        image: null,
-        category: "Naudanliharuoat",
-      },
-      {
-        featured: false,
-        dishNo: 1,
-
-        title: "Rapea kana hapanimeläkastikkeessa",
-        description:
-          "Kanaa paprikan, purjon, kevätsipulin ja seesamiöljyn kera, riisillä",
-        prices: [
-          {
-            title: null,
-            price: "16,00",
-          },
-        ],
-        dietary: "L, G",
-        image: null,
-        category: "Broileri- ja kanaruoat",
-      },
-      {
-        featured: false,
-        dishNo: 1,
-
-        title: "Rapea kana hapanimeläkastikkeessa",
-        description:
-          "Kanaa paprikan, purjon, kevätsipulin ja seesamiöljyn kera, riisillä",
-        prices: [
-          {
-            title: null,
-            price: "16,00",
-          },
-        ],
-        dietary: "L, G",
-        image: null,
-        category: "Broileri- ja kanaruoat",
-      },
-      {
-        featured: false,
-        dishNo: 1,
-
-        title: "Rapea kana hapanimeläkastikkeessa",
-        description:
-          "Kanaa paprikan, purjon, kevätsipulin ja seesamiöljyn kera, riisillä",
-        prices: [
-          {
-            title: null,
-            price: "16,00",
-          },
-        ],
-        dietary: "L, G",
-        image: null,
-        category: "Broileri- ja kanaruoat",
-      },
-      {
-        featured: false,
-        dishNo: 1,
-
-        title: "KungPao (sôt me)",
-        description:
-          "Parsakaali, paprika, porkkana, sipuli, tamarindi, purjo, chili ja cashew-pähkina, riisiä",
-        prices: [
-          {
-            title: "Katkarapu",
-            price: "18,90",
-          },
-          {
-            title: "Kana",
-            price: "15,50",
-          },
-          {
-            title: "Tofu",
-            price: "13,50",
-          },
-        ],
-        dietary: "L, G",
-        image: null,
-        category: "Merenelävät  ja Muut Ruokalajit",
-      },
-    ]);
-
-    const groupedDishes = computed(() => {
-      return Dishes.value.reduce((grouped, dish) => {
-        (grouped[dish.category] = grouped[dish.category] || []).push(dish);
-        return grouped;
-      }, {});
+const fetchDishes = async () => {
+  try {
+    loading.value = true;
+    error.value = null;
+    const response = await axios.post(API_URL, {
+      query: `
+        {
+          menus {
+            dishNumber
+            title
+            description
+            prices {
+              price
+              type
+            }
+            dietary
+            category
+            displayHome
+            bestSeller
+            image {
+              url
+            }
+            bestSellerIcon
+            hot
+            vegan
+            hiddenInMenu
+          }
+          categorynames{
+            categoryKey
+            title
+            sequence
+          }
+        }
+      `,
     });
 
-    return { groupedDishes };
-  },
+    if (response.data.errors) {
+      console.log("GraphQL errors:", response.data.errors);
+    } else {
+      dishes.value = response.data.data.menus;
+      category.value = response.data.data.categorynames;
+    }
+  } catch (e) {
+    error.value = e.message;
+  } finally {
+    loading.value = false;
+  }
 };
+
+onMounted(fetchDishes);
 </script>
 
 <style scoped></style>
