39aea9e7afd4021b705e20b8c877f3317a75e028
Phuoc Nguyen <phuoc94@hotmail.fi>
2023-08-18 00:01:59 +0300

    Update LunchBuffetSlide.vue to use data from the API.

--- src/components/LunchBuffetSlide.vue
+++ src/components/LunchBuffetSlide.vue
@@ -1,5 +1,6 @@
 <template>
   <div
+    v-if="content"
     class="relative flex min-h-screen items-center justify-center bg-cover bg-center"
   >
     <img
@@ -8,14 +9,61 @@
       class="absolute inset-0 z-0 h-full max-h-screen w-full object-cover"
     />
     <div class="relative z-10 mx-14 text-center text-white">
-      <h1 class="h1 mb-6 font-serif font-bold">Lunch Buffet</h1>
-      <p class="mb-8 font-inter">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt
-      </p>
-      <router-link :to="{ path: '/menu/lunchbuffet' }">
-        <button class="button">Buffet menu</button>
+      <h1 class="h1 mb-6 font-serif font-bold">{{ content.title }}</h1>
+      <p class="mb-8 max-w-3xl font-inter">{{ content.paragraph[0] }}</p>
+      <router-link :to="{ path: button.path }">
+        <button class="button">{{ button.label }}</button>
       </router-link>
     </div>
   </div>
 </template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import axios from "axios";
+import { getBrowserLanguage } from "@/utils/languageUtils";
+
+const API_URL = process.env.VUE_APP_API_URL;
+
+const content = ref(null); // Initialize content with null
+const loading = ref(true);
+const error = ref(null);
+const button = ref();
+
+const fetchContent = async () => {
+  try {
+    loading.value = true;
+    error.value = null;
+    const response = await axios.post(API_URL, {
+      query: `
+      query($locales: [Locale!]!){
+        contents(where: {contentId: "Lunch-Buffet-Slider"}, locales: $locales) {
+          title
+          paragraph
+        }
+        links(where: {customId: "Buffet-Menu-Button"}, locales: $locales) {
+          path
+          label
+        }
+      } 
+    `,
+      variables: {
+        locales: [getBrowserLanguage()],
+      },
+    });
+
+    if (response.data.errors) {
+      console.log("GraphQL errors:", response.data.errors);
+    } else {
+      content.value = response.data.data.contents[0];
+      button.value = response.data.data.links[0];
+    }
+  } catch (e) {
+    error.value = e.message;
+  } finally {
+    loading.value = false;
+  }
+};
+
+onMounted(fetchContent);
+</script>
