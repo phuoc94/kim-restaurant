9a5d99c46ba1cd2d0cec249264197eaf39cf3642
Phuoc Nguyen <phuoc94@hotmail.fi>
2023-08-18 00:34:07 +0300

    Update BestSellerSlide.vue to use new data fetching API.

--- src/components/BestSellerSlide.vue
+++ src/components/BestSellerSlide.vue
@@ -1,9 +1,12 @@
 <template>
   <div
+    v-if="content && !loading"
     class="flex min-h-screen flex-col justify-center bg-zinc-700 text-center"
   >
-    <h1 class="h1 mb-5 mt-8 font-serif font-bold text-white">Our Bestseller</h1>
-    <p class="font-inter text-white">Lorem ipsum dolor sit amet, consectetur</p>
+    <h1 class="h1 mb-5 mt-8 font-serif font-bold text-white">
+      {{ content.title }}
+    </h1>
+    <p class="font-inter text-white">{{ content.paragraph[0] }}</p>
 
     <div class="container my-8 flex flex-wrap justify-center gap-y-12 px-14">
       <div
@@ -20,13 +23,22 @@
       </div>
     </div>
 
-    <p class="mb-8 font-inter text-white">and more</p>
+    <p class="mb-8 font-inter text-white">{{ content.paragraph[1] }}</p>
 
     <div class="mb-12">
-      <router-link :to="{ path: '/menu/bestseller' }">
-        <button class="button">Menu</button>
+      <router-link :to="{ path: button.path }">
+        <button class="button">{{ button.label }}</button>
       </router-link>
     </div>
+  </div>
+  <div
+    v-else-if="loading"
+    class="flex min-h-screen items-center justify-center"
+  >
+    <p>Loading...</p>
+  </div>
+  <div v-else class="flex min-h-screen items-center justify-center">
+    <p>An error occurred: {{ error }}</p>
   </div>
 </template>
 
@@ -34,39 +46,56 @@
 import { ref, onMounted } from "vue";
 import Card from "@/components/BestSellerCard.vue";
 import axios from "axios";
+import { getBrowserLanguage } from "@/utils/languageUtils";
 const API_URL = process.env.VUE_APP_API_URL;
 
 const cards = ref([]);
+const content = ref();
+const button = ref();
 const loading = ref(true);
 const error = ref(null);
 
-const fetchDishes = async () => {
+const fetchData = async () => {
   try {
     loading.value = true;
     error.value = null;
     const response = await axios.post(API_URL, {
       query: `
-        {
+        query($locales: [Locale!]!){
           menus(first: 8, where: {displayHome: true}) {
-          title
-          description
-          prices(first: 1, orderBy: price_ASC) {
-            price
+            title
+            description
+            prices(first: 1, orderBy: price_ASC) {
+              price
+            }
+            image {
+              url(
+                transformation: {document: {output: {format: webp}}, image: {resize: {width: 300}}}
+                )
+            }
           }
-          image {
-            url(
-              transformation: {document: {output: {format: webp}}, image: {resize: {width: 300}}}
-              )
+          contents(where: {contentId: "Best-Seller-Slider"}, locales: $locales) {
+            title
+            paragraph
           }
-        }
+          links(where: {customId: "Best-Seller-Slider-Button"}, locales: $locales) {
+            path
+            label
+          }
         }
       `,
+      variables: {
+        locales: [getBrowserLanguage()],
+      },
     });
 
     if (response.data.errors) {
       console.log("GraphQL errors:", response.data.errors);
+      error.value = "GraphQL errors occurred";
     } else {
       cards.value = response.data.data.menus;
+      content.value = response.data.data.contents[0];
+      button.value = response.data.data.links[0];
     }
   } catch (e) {
     error.value = e.message;
@@ -75,5 +104,5 @@ const fetchDishes = async () => {
   }
 };
 
-onMounted(fetchDishes);
+onMounted(fetchData);
 </script>
