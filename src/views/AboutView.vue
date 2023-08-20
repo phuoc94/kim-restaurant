eb8ed16bd7feb23c6773b93699ddb99fe2511372
Phuoc Nguyen <phuoc94@hotmail.fi>
2023-08-20 23:31:18 +0300

    Updated AboutView.vue to use dynamic content from the API.

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,33 +1,21 @@
 <template>
-  <div>
+  <div v-if="content">
     <img class="max-h-screen w-full" src="@/assets/about_img.webp" />
     <div class="bg-custom-gradient py-24 md:py-32 lg:py-40">
       <h1 class="h1 text-center font-serif font-bold text-amber-400">
-        Kim Ravintola
+        {{ content.title }}
       </h1>
       <div
-        class="mx-auto mt-8 flex max-w-5xl flex-col gap-8 px-4 text-justify md:flex-row"
+        class="mx-auto mt-8 flex max-w-5xl flex-col gap-16 px-4 text-justify md:flex-row"
       >
-        <div>
+        <div class="flex-1">
           <p>
-            Lorem ipsum dolor sit amet consectetur. Bibendum purus vitae justo
-            ultrices cras a. Feugiat pharetra dictum orci ac elementum. Nulla
-            nec suspendisse tellus ante quam nisl montes vivamus. Aliquet urna
-            porta volutpat nibh nunc sagittis nisi. Nisi imperdiet arcu gravida
-            nullam. Integer euismod at morbi viverra semper enim. Sed hac risus
-            elementum auctor venenatis. Mauris pellentesque ultrices elit eget
-            id sed donec. Tristique feugiat etiam viverra enim.
+            {{ content.paragraph[0] }}
           </p>
         </div>
-        <div>
+        <div class="flex-1">
           <p>
-            Lorem ipsum dolor sit amet consectetur. Bibendum purus vitae justo
-            ultrices cras a. Feugiat pharetra dictum orci ac elementum. Nulla
-            nec suspendisse tellus ante quam nisl montes vivamus. Aliquet urna
-            porta volutpat nibh nunc sagittis nisi. Nisi imperdiet arcu gravida
-            nullam. Integer euismod at morbi viverra semper enim. Sed hac risus
-            elementum auctor venenatis. Mauris pellentesque ultrices elit eget
-            id sed donec. Tristique feugiat etiam viverra enim.
+            {{ content.paragraph[1] }}
           </p>
         </div>
       </div>
@@ -36,10 +24,47 @@
   </div>
 </template>
 
-<script lang="ts">
+<script setup>
+import axios from "axios";
+import { ref, onMounted } from "vue";
+import { getBrowserLanguage } from "@/utils/languageUtils";
 import FrameFour from "@/components/FrameFour.vue";
 
-export default {
-  components: { FrameFour },
+const API_URL = process.env.VUE_APP_API_URL;
+
+const content = ref(null);
+const loading = ref(true);
+const error = ref(null);
+
+const fetchContent = async () => {
+  try {
+    loading.value = true;
+    error.value = null;
+    const response = await axios.post(API_URL, {
+      query: `
+      query($locales: [Locale!]!){
+        contents(where: {contentId: "About-Section"}, locales: $locales) {
+          title
+          paragraph
+        }
+      } 
+    `,
+      variables: {
+        locales: [getBrowserLanguage()],
+      },
+    });
+
+    if (response.data.errors) {
+      console.log("GraphQL errors:", response.data.errors);
+    } else {
+      content.value = response.data.data.contents[0];
+    }
+  } catch (e) {
+    error.value = e.message;
+  } finally {
+    loading.value = false;
+  }
 };
+
+onMounted(fetchContent);
 </script>
