8245f34ec3b49e56d0351cf9b6f2bfec679f7dd3
Phuoc Nguyen <phuoc94@hotmail.fi>
2023-08-21 00:43:56 +0300

    Updated ContactView to use dynamic data from the API.

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,53 +1,52 @@
 <template>
   <div class="bg-image flex h-screen items-center justify-center px-4">
-    <div class="bg-zinc-700 px-16 py-12 md:px-20 lg:px-24">
+    <div v-if="translation" class="bg-zinc-700 px-16 py-12 md:px-20 lg:px-24">
       <div
         class="basis-full divide-y-4 divide-dashed text-center font-inter text-white md:basis-10/12 lg:basis-8/12 xl:basis-1/2"
       >
         <div class="pb-12">
-          <h1 class="h1 font-serif font-bold">Open Time</h1>
+          <h1 class="h1 font-serif font-bold">
+            {{ translation.contactTitle }}
+          </h1>
         </div>
-        <div class="flex justify-between py-12 text-lg">
-          <div>
-            <p class="font-bold">Ma</p>
-            <p>11:00 - 14:30</p>
-          </div>
-          <div>
-            <p class="font-bold">Ti - Pe</p>
-            <p>11:00 - 21:00</p>
-          </div>
-          <div>
-            <p class="font-bold">La</p>
-            <p>12:00 - 21:00</p>
-          </div>
-          <div>
-            <p class="font-bold">Su</p>
-            <p>12:00 - 20:30</p>
+        <div
+          class="flex justify-between py-12 text-lg"
+          v-if="openHours.length > 1"
+        >
+          <div v-for="hour in openHours" :key="hour.day">
+            <p class="font-bold">{{ hour.day }}</p>
+            <p>{{ hour.openHour }}</p>
           </div>
         </div>
         <div class="flex flex-col pt-6 text-left text-lg md:flex-row">
           <div class="mb-4 w-full md:mb-0 md:mr-2 md:w-7/12">
-            <p>We can be contacted via</p>
+            <p>{{ translation.ContactInfo }}</p>
             <p>
-              telephone on
-              <span class="font-bold text-amber-400">+358 40 5771358</span>
+              {{ translation.contactTelephoneText }}
+              <a :href="links[2].path"
+                ><span class="font-bold text-amber-400">{{
+                  links[2].label
+                }}</span></a
+              >
             </p>
             <p>
-              Facebook
+              {{ translation.contactFacebookText }}
               <a
-                href="https://www.facebook.com/VietnamilainenRavintolaKim"
+                :href="links[0].path"
                 target="_blank"
                 class="font-bold text-amber-400"
               >
-                @Vietnamilainen Ravintola Kim</a
+                {{ links[0].label }}</a
               >
             </p>
           </div>
           <div class="w-full md:ml-2 md:mt-0 md:w-5/12">
             <p>
-              We are located at
-              <span class="font-bold text-amber-400"
-                >Kaskenkatu 8, Turku, Finland</span
+              {{ translation.contactLocationText }}
+              <a :href="links[1].path" target="_blank"
+                ><span class="font-bold text-amber-400">{{
+                  links[1].label
+                }}</span></a
               >
             </p>
           </div>
@@ -58,12 +57,70 @@
   <FrameFour />
 </template>
 
-<script lang="ts">
+<script setup>
+import { ref, onMounted } from "vue";
+import axios from "axios";
+
 import FrameFour from "@/components/FrameFour.vue";
 
-export default {
-  components: { FrameFour },
+import { getBrowserLanguage } from "@/utils/languageUtils";
+import { convertToTranslationObject } from "@/utils/convertToTranslationObject";
+
+const API_URL = process.env.VUE_APP_API_URL;
+
+const links = ref([]);
+const openHours = ref([]);
+const translation = ref();
+const loading = ref(true);
+const error = ref(null);
+
+const fetchLocales = async () => {
+  try {
+    loading.value = true;
+    error.value = null;
+    const response = await axios.post(API_URL, {
+      query: `
+        query($locales: [Locale!]!){
+          links(
+            locales: $locales,
+            orderBy: customId_ASC
+            where: {OR: [{customId: "phoneNumber"}, {customId: "Location"}, {customId: "Facebook"}]}
+          ) {
+            path
+            label
+          }
+          translationItems(locales: $locales, where: {customId_contains: "contact"}) {
+            customId
+            text
+          }
+          openTimes(locales: $locales, orderBy: weekDaysNumber_ASC ) {
+            day
+            openHour
+          }
+        }
+      `,
+      variables: {
+        locales: [getBrowserLanguage()],
+      },
+    });
+
+    if (response.data.errors) {
+      console.error("GraphQL errors:", response.data.errors);
+    } else {
+      openHours.value = response.data.data.openTimes;
+      links.value = response.data.data.links;
+      translation.value = convertToTranslationObject(
+        response.data.data.translationItems
+      );
+    }
+  } catch (e) {
+    error.value = e.message;
+  } finally {
+    loading.value = false;
+  }
 };
+
+onMounted(fetchLocales);
 </script>
 
 <style scoped>
