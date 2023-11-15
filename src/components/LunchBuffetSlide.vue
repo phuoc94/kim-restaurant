eb1f40a316c8a9d4f69ad3833b6836e3dcbf9a81
Phuoc Nguyen <phuoc94@hotmail.fi>
2023-11-15 19:12:49 +0000

    Refactor LunchBuffetSlide component to use unordered list and add new content section.

--- src/components/LunchBuffetSlide.vue
+++ src/components/LunchBuffetSlide.vue
@@ -8,13 +8,31 @@
       alt="Lunch Buffet"
       class="absolute inset-0 z-0 h-full max-h-screen w-full object-cover"
     />
-    <div class="relative z-10 mx-14 text-center text-white">
+    <div class="relative z-10 mx-14 text-white">
       <h1 class="h1 mb-6 font-serif font-bold">{{ content.title }}</h1>
-      <div class="mb-8 max-w-3xl font-inter">
-        <p v-for="(item, index) in content.paragraph" :key="index">
-          {{ item }}
-        </p>
+      <div class="mb-8 max-w-2xl font-inter font-bold">
+        <ul>
+          <li
+            v-for="(item, index) in content.paragraph"
+            :key="index"
+            class="flex gap-1"
+          >
+            <div>
+              <i class="pi pi-check" style="font-weight: bolder"></i>
+            </div>
+            <div>{{ item }}</div>
+          </li>
+        </ul>
       </div>
+      <div>
+        <div class="mb-8 max-w-2xl font-inter font-bold">
+          <p
+            class="font-montserrat"
+            v-html="content.pagas[0].paragraph.html"
+          ></p>
+        </div>
+      </div>
+
       <router-link :to="{ path: content.buttons[0].path }">
         <button class="button">{{ content.buttons[0].label }}</button>
       </router-link>
@@ -23,8 +41,10 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue";
+import { onMounted, ref } from "vue";
+
 import axios from "axios";
+
 import { getBrowserLanguage } from "@/utils/languageUtils";
 
 const API_URL = process.env.VUE_APP_API_URL;
@@ -47,6 +67,11 @@ const fetchContent = async () => {
             path
             label
           }
+          pagas{
+            paragraph{
+              html
+            }
+          }
         }
       } 
     `,
@@ -59,7 +84,7 @@ const fetchContent = async () => {
       console.log("GraphQL errors:", response.data.errors);
     } else {
       content.value = response.data.data.contents[0];
-      console.log(response.data.data.contents[0]);
+      console.log(content.value.pagas[0].paragraph);
     }
   } catch (e) {
     error.value = e.message;
